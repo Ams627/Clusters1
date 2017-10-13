dc62c93f77631f6c2ed69fa6f91f996f554d713c
Adrian Sims <asims@parkeon.com>
2017-10-13 17:58:21 +0100

    missed files

--- ReadClusters/Program.cs
+++ ReadClusters/Program.cs
@@ -7,12 +7,50 @@ using System.Threading.Tasks;
 
 namespace ReadClusters
 {
+
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var clusterDic = new Dictionary<string, List<string>>();
             try
             {
+                var filename1 = "s:\\RJFAF628.FSC";
+                var filename2 = "s:\\RJFAF628.FFL";
+
+                // read the clusters file:
+                foreach (var line in System.IO.File.ReadLines(filename1))
+                {
+                    if (line.Length > 8)
+                    {
+                        var cluster = line.Substring(1, 4);
+                        var member = line.Substring(5, 4);
+                        DictUtils.AddEntry(clusterDic, cluster, member);
+                    }
+                }
+
+                // read the flow file:
+                var nlcCodes = new HashSet<string>();
+                foreach (var line in System.IO.File.ReadLines(filename2))
+                {
+                    if (line.Length > 2 & line[0] == 'R' && line[1] == 'F')
+                    {
+                        nlcCodes.Add(line.Substring(2, 4));
+                        nlcCodes.Add(line.Substring(6, 4));
+                    }
+                }
+
+                var finalCodes = new HashSet<string>();
+
+                foreach (var code in nlcCodes)
+                {
+                    if (nlcCodes.Contains())
+                    {
+
+                    }
+                }
+
+                    Console.WriteLine();
             }
             catch (Exception ex)
             {
